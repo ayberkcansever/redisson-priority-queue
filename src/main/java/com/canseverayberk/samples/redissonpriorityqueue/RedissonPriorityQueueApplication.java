63e360e4abad88cae5bfd5976b154430b3d81710
Ayberk Cansever <>
2021-03-21 16:30:57 +0300

    first commit

---
+++ src/main/java/com/canseverayberk/samples/redissonpriorityqueue/RedissonPriorityQueueApplication.java
@@ -0,0 +1,13 @@
+package com.canseverayberk.samples.redissonpriorityqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonPriorityQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedissonPriorityQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canseverayberk/samples/redissonpriorityqueue/RedissonPriorityQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canseverayberk.samples.redissonpriorityqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonPriorityQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
